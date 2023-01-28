cc7cc3033f876457d26060f71a62ac491efa321f
duduhazan <duduhazan123@gmail.com>
2023-01-28 18:51:28 +0200

    fix logo import

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,6 +1,6 @@
 import "./style.css";
 import { useNavigate } from "react-router-dom";
-import logo from "../../../public/logo.png";
+import logo from "../../assets/logo.png";
 
 export const Header = (props) => {
   const navigate = useNavigate();
